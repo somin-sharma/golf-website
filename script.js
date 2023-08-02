a79ca711d4261314ad9a942793bb3700d94ff5bf
somin-sharma <sominsharma0000@gmail.com>
2023-08-02 20:36:20 +0530

    half

---
+++ script.js
@@ -0,0 +1,37 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets) {
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x-150+"px"
+    blur.style.top = dets.y-150+"px"
+
+
+})
+gsap.to("#nav",{
+    backgroundColor :"#000",
+    height:"110px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        markers:true,
+        start: "top -10% ",
+        end:"top -11%",
+        scrub:-1
+    }
+})
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        markers: true,
+        start: "top -60%",
+        end: "top -100%",
+        scrub:2
+
+    }
+
+})
